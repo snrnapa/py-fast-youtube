463603b99e2353363ac0f59db2663b4762417df3
snrnapa <shino.satoru@gmail.com>
2024-01-30 00:09:19 +0900

    fastでyoutubedlのAPIを作成した

---
+++ README.md
@@ -0,0 +1,7 @@
+## Getting Start
+pip3 install -r requirements.txt
+
+uvicorn main:app --reload 
+
+## pip check
+pip freeze
---
+++ main.py
@@ -0,0 +1,26 @@
+from fastapi import FastAPI
+from yt_dlp import YoutubeDL
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+@app.get("/yt_donwload")
+def read_item():
+    f = open("dllist.txt", "r")
+    datalist = f.readlines()
+    # 動画用
+    ydl_opts = {"format": "best"}
+    # 音声用
+    # ydl_opts = {'format': 'bestaudio/best'}
+
+    for data in datalist:
+        print("***********開始*******************")
+        print(data)
+        with YoutubeDL(ydl_opts) as ydl:
+            result = ydl.download([data])
+            print(result)
