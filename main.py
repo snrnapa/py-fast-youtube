0d5fc597f030afbcfb87682fc22f4234d22ab8e8
snrnapa <shino.satoru@gmail.com>
2024-01-30 21:46:25 +0900

    getメソッドでファイルがダウンロードできるようにAPIを実装した

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-from fastapi import FastAPI
+from pathlib import Path
 from yt_dlp import YoutubeDL
+from datetime import datetime
+
+import uvicorn
+from fastapi import FastAPI
+from fastapi.responses import FileResponse
 
 app = FastAPI()
 
@@ -24,3 +29,22 @@ def read_item():
         with YoutubeDL(ydl_opts) as ydl:
             result = ydl.download([data])
             print(result)
+
+
+@app.get("/get_file/{filename:path}")
+async def get_file(filename: str):
+    """任意ファイルのダウンロード"""
+    current = Path()
+    file_path = current / "files" / filename
+
+    now = datetime.now()
+
+    response = FileResponse(
+        path=file_path, filename=f"download_{now.strftime('%Y%m%d%H%M%S')}_{filename}"
+    )
+
+    return response
+
+
+if __name__ == "__main__":
+    uvicorn.run("app:app", host="127.0.0.1", port=8000, reload=True)
