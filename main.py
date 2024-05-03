b566d4a3d59f7f83deb9d52e443d65744aadfe38
snrnapa <shino.satoru@gmail.com>
2024-05-03 11:13:39 +0900

    cors設定を見直した

--- main.py
+++ main.py
@@ -18,9 +18,9 @@ app = FastAPI()
 # CORSを回避するために追加（今回の肝）
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://vite-ppsldzjoc-snrnapas-projects.vercel.app"],
     allow_credentials=True,  # 追記により追加
-    allow_methods=["*"],  # 追記により追加
+    allow_methods=["GET", "POST", "PUT", "DELETE"],  # 追記により追加
     allow_headers=["*"],  # 追記により追加
 )
 
