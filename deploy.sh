90bd77ebfcc7e941b5ecff88e7cbba81d44a25d4
snrnapa <shino.satoru@gmail.com>
2024-07-28 22:42:00 +0900

    main.pyから起動に伴い、shを修正

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-cd /home/napa/prod/ydl/py-fast-youtube && pm2 delete "ydl-back" && git checkout master && git pull && pm2 start "python3 -m uvicorn main:app --reload" --name "ydl-back"
+cd /home/napa/prod/ydl/py-fast-youtube
+pm2 delete "ydl-back"
+git checkout master
+git pull
+pm2 start "python3 main.py" --name "ydl-back"
